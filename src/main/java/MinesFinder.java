e3a5afbe65cd0b0c5ee11aaae4423f636776f7d8
André Pinto <2201723@my.ipleiria.pt>
2022-03-09 10:10:34 +0000

    configurar painel

--- src/main/java/MinesFinder.java
+++ src/main/java/MinesFinder.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MinesFinder {
+import javax.swing.*;
+
+public class MinesFinder extends JFrame {
+
+    private JPanel painelPrincipal;
+
+    public MinesFinder(String title) {
+        super(title);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+        pack();
+    }
+
+    public static void main(String[] args) { new MinesFinder("Mines Finder").setVisible(true); }
 }
